967d985a652511296e1fea7de3c2383c60bfbd3c
Arun Jose <arun.jose@gmail.com>
2019-10-31 03:00:03 +0000

    Use config parameter is_site to determing which classifier to use

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -44,8 +44,8 @@ class TLDetector(object):
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
-        #self.is_sim = self.config['is_sim']
-        self.is_sim = True
+        self.is_sim = not self.config['is_site']
+        #self.is_sim = True
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
